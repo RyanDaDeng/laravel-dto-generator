d343f99eb2458c341166dfa46afe38f3e1616149
RyanDaDeng <37111049+RyanDaDeng@users.noreply.github.com>
2019-08-12 12:16:21 +1000

    Update AbstractClassGenerator.php

--- src/Services/AbstractClassGenerator.php
+++ src/Services/AbstractClassGenerator.php
@@ -4,7 +4,9 @@ namespace TimeHunter\LaravelJsonToClassGenerator\Services;
 
 
 use Illuminate\Support\Facades\File;
+use Nette\PhpGenerator\ClassType;
 use Nette\PhpGenerator\PhpFile;
+use Nette\PhpGenerator\PhpNamespace;
 
 /**
  * Class JsonToClassGenerator
@@ -25,6 +27,66 @@ abstract class AbstractClassGenerator
         $this->recursiveCreateFile($this->getData(), config('jsontoclassgenerator.namespace'));
     }
 
+    public function getType($value)
+    {
+
+        if ($value === null) {
+            return;
+        }
+
+        if (is_string($value)) {
+            return 'string';
+        }
+
+        if (($value === true || $value === false) === true) {
+            return 'bool';
+        }
+
+        if (is_int($value)) {
+            return 'int';
+        }
+
+        if (is_float($value)) {
+            return 'float';
+        }
+        return;
+    }
+
+    public function addMethod(ClassType $class, $type, $name,$isArray = false)
+    {
+
+        $realType = last(explode('\\', $type));
+
+        $getComment = empty($realType) ? 'mixed' : $realType;
+
+
+        if($isArray === true){
+            $class->addMethod('get' . ucfirst($name))
+                ->addComment('@return ' . $getComment)
+                ->setBody('return $this->' . $name . ';');
+
+            $class->addMethod('set' . ucfirst($name))
+                ->setReturnType('self')
+                ->addComment('@param ' . $realType . ' $' . $name . "\n" . '@return $this')
+                ->setBody('$this->' . $name . ' = ' . '$' . $name . ';' . "\n" . 'return $this;')
+                ->addParameter($name);
+        }else{
+            $class->addMethod('get' . ucfirst($name))
+                ->setReturnType($type)
+                ->addComment('@return ' . $getComment)
+                ->setBody('return $this->' . $name . ';');
+
+            $class->addMethod('set' . ucfirst($name))
+                ->setReturnType('self')
+                ->addComment('@param ' . $realType . ' $' . $name . "\n" . '@return $this')
+                ->setBody('$this->' . $name . ' = ' . '$' . $name . ';' . "\n" . 'return $this;')
+                ->addParameter($name)
+                ->setTypeHint($type);
+        }
+
+
+    }
+
     /**
      * @param $sample
      * @param $namespaceString
@@ -32,11 +94,11 @@ abstract class AbstractClassGenerator
     private function recursiveCreateFile($sample, $namespaceString)
     {
         foreach ($sample as $key => $data) {
+
             $className = $this->convertClassName($key);
             $phpFile   = new PhpFile();
             $namespace = $phpFile->addNamespace($namespaceString);
             $class     = $namespace->addClass($className);
-
             $class->addMethod('create')->setStatic(true)->setBody('return new self;')->addComment('@return ' . $className);
             $toArray = "return [\n";
             foreach ($data as $itemKey => $item) {
@@ -44,25 +106,47 @@ abstract class AbstractClassGenerator
 
                 if (is_object($item)) {
                     $subClassName = $this->convertClassName($itemKey);
-
-                    $class->addProperty($camelCase)->addComment('@var ' . ucfirst($camelCase) . ' $' . $camelCase);
-                    $class->addMethod('add' . ucfirst($camelCase))->addComment('@param ' . ucfirst($camelCase) . ' $' . $camelCase . "\n" . '@return $this')->setBody('$this->' . $camelCase . ' = ' . '$' . $camelCase . ';' . "\n" . 'return $this;')->addParameter($camelCase);
+                    $namespace->addUse($namespaceString . '\\' . ucfirst($camelCase), ucfirst($camelCase));
+                    $class->addProperty($camelCase)->setVisibility('private')->addComment('@var ' . ucfirst($camelCase) . ' $' . $camelCase);
+                    $class->addMethod('add' . ucfirst($camelCase))
+                        ->setReturnType('self')
+                        ->addComment('@param ' . $camelCase . ' $' . $camelCase . "\n" . '@return $this')
+                        ->setBody('$this->' . $camelCase . ' = ' . '$' . $camelCase . ';' . "\n" . 'return $this;')
+                        ->addParameter($camelCase)
+                        ->setTypeHint($namespaceString . '\\' . ucfirst($camelCase));
 
                     $toArray .= "   '$itemKey' => " . '$this->' . $camelCase . '->toArray()' . ',' . "\n";
+                    $this->addMethod($class, $namespaceString . '\\' . ucfirst($camelCase), $camelCase);
                     $this->recursiveCreateFile([$itemKey => $item], $namespaceString);
                 } else {
+
                     if (is_array($item)) {
                         $subClassName = $this->convertClassName($itemKey);
+                        $namespace->addUse($namespaceString . '\\' . ucfirst($camelCase), ucfirst($camelCase));
+                        $class->addProperty($camelCase,
+                            [])->addComment('@var ' . ucfirst($camelCase) . '[] $' . $camelCase)->setVisibility('private');
+                        $class->addMethod('add' . ucfirst($camelCase))
+                            ->setReturnType('self')
+                            ->addComment('@param ' . $camelCase . ' $' . $camelCase . "\n" . '@return $this')
+                            ->setBody('$this->' . $camelCase . '[] = ' . '$' . $camelCase . ';' . "\n" . 'return $this;')
+                            ->addParameter($camelCase)
+                            ->setTypeHint($namespaceString . '\\' . ucfirst($camelCase));
+//
+//                        $toArray .= "   '$itemKey' => " . 'collect($this->' . $camelCase . ')->map(function (' . ucfirst($camelCase) . ' $data){
+//        return $data->toArray();
+//    })->toArray()' . ',' . "\n";
 
-                        $class->addProperty($camelCase)->addComment('@var ' . ucfirst($camelCase) . ' $' . $camelCase);
-                        $class->addMethod('add' . ucfirst($camelCase))->addComment('@param ' . ucfirst($camelCase) . ' $' . $camelCase . "\n" . '@return $this')->setBody('$this->' . $camelCase . '[] = ' . '$' . $camelCase . ';' . "\n" . 'return $this;')->addParameter($camelCase);
+                        $toArray .="   '$itemKey' => " ."array_map(function (".ucfirst($camelCase). ' $data){
+          return $data->toArray();
+      }, $this->'.$camelCase.")" .',' . "\n";
 
-                        $toArray .= "   '$itemKey' => " . 'collect($this->' . $camelCase . ')->map(function (' . "$subClassName" . ' $data){
-        return $data->toArray();
-    })->toArray()' . ',' . "\n";
+
+                        $this->addMethod($class, $namespaceString . '\\' . ucfirst($camelCase).'[]', $camelCase,true);
                         $this->recursiveCreateFile([$itemKey => $item], $namespaceString);
                     } else {
-                        $class->addProperty($camelCase)->addComment('@var $' . $camelCase);
+                        $propertyType = $this->getType($item);
+                        $class->addProperty($camelCase)->setVisibility('private')->addComment('@var ' . $propertyType . ' $' . $camelCase);
+                        $this->addMethod($class, $propertyType, $camelCase, $namespaceString);
                         $toArray .= "   '$itemKey' => " . '$this->' . $camelCase . ',' . "\n";
                     }
                 }
